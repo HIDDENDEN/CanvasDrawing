f95d3ca7c7df4ef4c35ae887f8c8a1a4c2e986a1
Hiddenden <leluhinsu@gmail.com>
2022-03-22 17:50:23 +0300

    Updates

--- app/src/main/java/com/example/canvasdrawing/CustomView/MyCanvasView.kt
+++ app/src/main/java/com/example/canvasdrawing/CustomView/MyCanvasView.kt
@@ -2,37 +2,32 @@ package com.example.canvasdrawing.CustomView
 
 import android.content.Context
 import android.graphics.*
+import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
+import com.example.canvasdrawing.DecoratorHelper
 import com.example.canvasdrawing.R
 
 private const val STROKE_WIDTH = 12f // has to be float
 
 
-class MyCanvasView(context: Context): View(context) {
+class MyCanvasView @JvmOverloads constructor(
+    context: Context?,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr)  {
     private lateinit var extraCanvas: Canvas
     private lateinit var extraBitmap: Bitmap
-    private val backgroundColor = ResourcesCompat.getColor(resources,
-        android.R.color.holo_purple, null)
-    private val drawColor = ResourcesCompat.getColor(resources, android.R.color.holo_red_dark, null)
+
+
     private var path = Path()
     private var motionTouchEventX = 0f
     private var motionTouchEventY = 0f
     private var currentX = 0f
     private var currentY = 0f
-    private val paint = Paint().apply {
-        color = drawColor
-        // Smooths out edges of what is drawn without affecting shape.
-        isAntiAlias = true
-        // Dithering affects how colors with higher-precision than the device are down-sampled.
-        isDither = true
-        style = Paint.Style.STROKE // default: FILL
-        strokeJoin = Paint.Join.ROUND // default: MITER
-        strokeCap = Paint.Cap.ROUND // default: BUTT
-        strokeWidth = 50.0f // default: Hairline-width (really thin)
-    }
+
     private val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
     private lateinit var frame: Rect
 
@@ -43,7 +38,7 @@ class MyCanvasView(context: Context): View(context) {
         if (::extraBitmap.isInitialized) extraBitmap.recycle()
         extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
         extraCanvas = Canvas(extraBitmap)
-        extraCanvas.drawColor(backgroundColor)//specifying color in which we will draw on canvas
+        extraCanvas.drawColor(backgroundColorPurple)//specifying color in which we will draw on canvas
         // Calculate a rectangular frame around the picture.
         val inset = 40
 //        frame = Rect(inset, inset, width - inset, height - inset)
@@ -85,7 +80,7 @@ class MyCanvasView(context: Context): View(context) {
             currentX = motionTouchEventX
             currentY = motionTouchEventY
             // Draw the path in the extra bitmap to cache it.
-            extraCanvas.drawPath(path, paint)
+            extraCanvas.drawPath(path, DecoratorHelper.paint)
         }
         invalidate()
     }
@@ -93,6 +88,20 @@ class MyCanvasView(context: Context): View(context) {
     private fun touchUp() {
         // Reset the path so it doesn't get drawn again.
         path.reset()
+    }
+
+    private val backgroundColorPurple = ResourcesCompat.getColor(resources, android.R.color.holo_purple, null)
+    private val backgroundColorGrey = ResourcesCompat.getColor(resources, R.color.MyGrey, null)
+    private val backgroundColorOrange = ResourcesCompat.getColor(resources, R.color.MyOrange, null)
+    private val backgroundColorBlue = ResourcesCompat.getColor(resources, R.color.MyBlue, null)
+    private val backgroundColorRed = ResourcesCompat.getColor(resources, R.color.MyRed, null)
+
+    private var resetCounter = 0
+    val backgroundColorList = mutableListOf<Int>(backgroundColorPurple,backgroundColorOrange,backgroundColorBlue, backgroundColorRed,backgroundColorGrey)
+    fun resetCanvas(){
+        resetCounter+=1
+        extraCanvas.drawColor(backgroundColorList.get(resetCounter % backgroundColorList.size))
+        invalidate()
     }
 
 
